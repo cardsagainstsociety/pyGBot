2507a909107daf5e55b46efa10b98591c78faaa8
Aleksandr Soborov <asoborov@gmail.com>
2012-04-08 19:24:39 -0500

    Implemented custom cards in their own file.

---
+++ pyGBot/Plugins/games/CardsAgainstHumanityCustom.py
@@ -0,0 +1,28 @@
+# Place any custom cards in this file
+# to be loaded on top of the main deck.
+#
+# Ensure the formats are correct, or else
+# the bot may crash during runtime when 
+# invalid cards are chosen.
+
+CUSTBLACKCARDS = [
+# Current format:
+# ['Card name as a string', Cards to play]
+#
+# If you want to use fill-in-the-blank,
+# make the intended blank EXACTLY four
+# underscores long. If no blanks are
+# specified, only the played cards
+# will display.
+#
+# ['Sample fill in the ____', 1],
+# ['There are no blanks', 2],
+# ['____ has multiple ____ for ____', 3],
+
+]
+
+CUSTWHITECARDS = [
+# Current format:
+# 'Example Card',
+
+]
